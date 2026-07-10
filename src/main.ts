753c54adbc21ba68ef9a365f63dc092f16c2466f
nickbyrum2469 <nickbyrum2469@gmail.com>
2026-07-10 11:02:41 -0600

    hotfix: install final static visual polish before freezing

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { CombatFeelDirector } from "./core/CombatFeelDirector.js";
 import { FloorTwoArrivalDirector } from "./core/FloorTwoArrivalDirector.js";
 import { FrontierContractDirector } from "./core/FrontierContractDirector.js";
 import { PerformanceDirector } from "./core/PerformanceDirector.js";
+import { VisualPolishDirector } from "./core/VisualPolishDirector.js";
 import { VisualRecoveryDirector } from "./core/VisualRecoveryDirector.js";
 import { ExpeditionJournal } from "./ui/ExpeditionJournal.js";
 import { LoadoutOverlay } from "./ui/LoadoutOverlay.js";
@@ -177,6 +178,7 @@ const boot = async (): Promise<void> => {
     installFloorTwoSafety(game, floorTwo);
     new FrontierContractDirector(game);
     new VisualRecoveryDirector(game);
+    new VisualPolishDirector(game);
     consolidateFloorTwoStaticGeometry(game);
     new PerformanceDirector(engine, game.world, renderer);
     new CombatFeelDirector(game, engine);
