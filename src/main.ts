26e8e78a62e7188d006decc6c27d918e660bd17d
nickbyrum2469 <nickbyrum2469@gmail.com>
2026-07-10 08:03:46 -0600

    perf: wire adaptive director into game boot

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./styles.css";
 import * as BabylonModule from "babylonjs";
+import { PerformanceDirector } from "./core/PerformanceDirector.js";
 import { ExpeditionJournal } from "./ui/ExpeditionJournal.js";
 import { LoadoutOverlay } from "./ui/LoadoutOverlay.js";
 
@@ -83,6 +84,7 @@ const boot = async (): Promise<void> => {
     new ExpeditionJournal();
     new LoadoutOverlay();
     game.world.mara.root.position.y += 0.31;
+    new PerformanceDirector(engine, game.world, renderer);
     game.run();
   } catch (error) {
     console.error(error);
